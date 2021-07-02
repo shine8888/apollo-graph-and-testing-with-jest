b65e37967c091f2576cb5834217f0213dd1db65c
QuangKieu <muathuvang8880@gmail.com>
2021-07-02 17:36:31 +0700

    add jsdoc and unit test

--- Schema/Resolvers.js
+++ Schema/Resolvers.js
@@ -1,16 +1,34 @@
-const {data} = require('../data')
-const resolvers = {
-    Query: {
-        getAllUser() {
-            return data
-        }
-    },
-    Mutation :{
-        createUser(parent, args){
-            data.push(args)
-            return args
-        }
-    }
-}
+// Getting data from database
+const { data } = require("../data");
 
-module.exports = {resolvers}
+/**
+ * Init the resolvers with Query and Mutation
+ */
+const resolvers = {
+	/**
+	 * Get all data from database
+	 * @returns {[Int]} - An array of int
+	 */
+	Query: {
+		getData() {
+			return data;
+		},
+	},
+	/**
+	 * Get sum of an array
+	 * @param {Int} args - The number added to the array
+	 * @returns {Int} - The sum of array and new added number
+	 */
+	Mutation: {
+		getSum(args) {
+			var sum = 0;
+			data.forEach((element) => {
+				sum += element;
+			});
+			sum = sum + args.a;
+			return parseInt(sum);
+		},
+	},
+};
+
+module.exports = { resolvers };
--- graph.test.js
+++ graph.test.js
@@ -1,64 +1,9 @@
-const fs = require("fs");
-const { makeExecutableSchema } = require("graphql-tools");
-const { graphql } = require("graphql");
-// Import resolvers and typedefs
 const { resolvers } = require("./Schema/Resolvers");
-const { typeDefs } = require("./Schema/TypeDefs");
-// the data test
-const { data } = require("./data");
-
-// a structure for test cases
-const allUsersTestCase = {
-	id: "All users",
-	query: `
-      query {
-        getAllUser{
-           name
-           age
-           address
-        }
-      }
-    `,
-	variables: {},
-
-	// injecting the users
-	context: { users: data },
-
-	// expected result
-	expected: {
-		data: {
-			getAllUser: [
-				{
-					name: "Shine",
-					age: 22,
-					address: "Hanoi",
-				},
-				{
-					name: "Kate",
-					age: 24,
-					address: "Bacgiang",
-				},
-				{
-					name: "Cathy",
-					age: 26,
-					address: "Co",
-				},
-			],
-		},
-	},
-};
-
-describe("My Test Cases", () => {
-	// array of all test cases
-	const cases = [allUsersTestCase];
-	// make the actual schema
-	const schema = makeExecutableSchema({ typeDefs, resolvers });
-	// running the test for each case in the cases array
-	cases.forEach((obj) => {
-		const { id, query, variables, context, expected } = obj;
-		test(`query:${id}`, async () => {
-			const result = await graphql(schema, query, null, context, variables);
-			return expect(result).toEqual(expected);
-		});
+/**
+ * Function test the total sum of an array
+ */
+describe("getTotalSum", () => {
+	test("should return the total sum of an array", () => {
+		expect(resolvers.Mutation.getSum({ a: 5 })).toBe(30);
 	});
 });
